12130d467538d0add38269ef76657ddd9d82d8a6
7771 <32600937+7771@users.noreply.github.com>
2017-10-20 21:27:40 -0700

    Update maze.js

--- maze.js
+++ maze.js
@@ -9,17 +9,24 @@ maze.addEventListener("onmouseover", function(){
 onmouseover = "myFunction()"
 function myFunction(){
 document.body.getElementById("boundary1").setAttribute("youlose","changeColor");
-});
+})};
+                      
+var x;                    
+                    
 function load = {}
 window.onload = load;
 
-function divFunction() {
-    var x = document.querySelectorAll("boundary");
-    var i;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.backgroundColor = "red";
+var count;
+var elements = document.querySelectorAll("div");
+
+for ( var i=0; i<elements.length; i++) {
+    elements[i].hover(function(){
+        $(this).css("color", "red");
+        count+=1;
+    }, 
+        function(){
+    $(this).css("color", "black")
     }
-}
 </script>
 
 
