dd019702e475e38942b4bc57b583aaf0cd40fe4a
7771 <32600937+7771@users.noreply.github.com>
2017-10-20 19:13:53 -0700

    Update and rename mouseover to maze.js

---
+++ maze.js
@@ -0,0 +1,25 @@
+<script>
+
+<style>
+.changeColor{color:red;}
+</style>
+
+maze.addEventListener("onmouseover", function(){
+
+onmouseover = "myFunction()"
+function myFunction(){
+document.body.getElementById("boundary1").setAttribute("youlose","changeColor");
+});
+function load = {}
+window.onload = load;
+
+$( document.body ).hover(function() {
+  $( "div" ).each(function( i ) {
+    this.color = "red" ) {
+      this.style.color = "black";
+    } 
+  });
+});
+</script>
+
+
